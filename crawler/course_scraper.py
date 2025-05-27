d33b81864cee80fbc4d95864172dbb342ca1b8c0
deemo-a <deemoasia07@gmail.com>
2025-05-27 18:21:16 +0800

    Update course_scraper.py

--- crawler/course_scraper.py
+++ crawler/course_scraper.py
@@ -1,4 +1,4 @@
-import requests
+-import requests
 from bs4 import BeautifulSoup
 from .portal_scraper import PortalScraper
 
@@ -33,7 +33,6 @@ class CoursePKUScraper(PortalScraper):
             assignments.append({
                 "title": title,
                 "description": description,
-                "due_date": due,
-                "source": "PKU Course"
+                "due_date": due
             })
         return assignments
