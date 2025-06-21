0a517a90d3ece731effb5b23d2eb14616815b801
deemo-a <deemoasia07@gmail.com>
2025-06-21 20:38:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-import ctypes
+import os
 import sys
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
-def relaunch_as_admin():
-    params = " ".join([f'"{arg}"' for arg in sys.argv])
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
-    sys.exit()
-
-if not is_admin():
-    relaunch_as_admin()
-
-
 import sqlite3
 import tkinter as tk
+import time
 
 from data.database import DatabaseManager
 from crawler.canvas_scraper import CanvasScraper
@@ -25,6 +10,22 @@ from crawler.course_scraper import CoursePKUScraper
 from crawler.openjudge_scraper import OpenJudgeScraper
 from backend.controller import Controller, DebugController
 from gui.main_interface import create_main_interface
+
+
+def ensure_admin():
+    if sys.platform == 'win32':
+        import ctypes
+        if not ctypes.windll.shell32.IsUserAnAdmin():
+            params = " ".join([f'"{arg}"' for arg in sys.argv])
+            # pythonw.exe 无控制台窗口
+            pythonw = sys.executable.replace("python.exe", "pythonw.exe")
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", pythonw, params, None, 1)
+            sys.exit()
+    else:
+        if os.geteuid() != 0:
+            print("当前非 root 用户，尝试用 sudo 重新运行...")
+            params = [sys.executable] + sys.argv
+            os.execvp('sudo', ['sudo', '-E'] + params)
 
 
 def main():
@@ -55,4 +56,5 @@ def main():
 
 
 if __name__ == "__main__":
+    ensure_admin()
     main()
