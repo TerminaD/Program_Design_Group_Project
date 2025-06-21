f9e2a0188676291d1832c8bc1c0ea6b6645b7a79
deemo-a <deemoasia07@gmail.com>
2025-06-21 20:15:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
 import tkinter as tk
+import ctypes
+import sys
 
 from data.database import DatabaseManager
 from crawler.canvas_scraper import CanvasScraper
@@ -10,6 +12,19 @@ from gui.main_interface import create_main_interface
 
 
 def main():
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    def relaunch_as_admin():
+        params = " ".join([f'"{arg}"' for arg in sys.argv])
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
+        sys.exit()
+
+    if not is_admin():
+        relaunch_as_admin()
     # 在 debug 模式下，不进行爬虫，使用一个占位数据库
     is_debug = False
 
@@ -19,7 +34,7 @@ def main():
     db.initialize()
 
     app = tk.Tk()
-    
+
     # Initialize crawler and controller
     if is_debug:
         controller = DebugController(db)
